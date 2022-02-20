0b46dd3b4da02aad9157e4d25c833b8ecd319ef0
Peter Joder <peter.joder1@gibb.ch>
2022-02-20 19:59:37 +0100

    Changed title font style

--- JasminScheidegger-Client/layouts/default.vue
+++ JasminScheidegger-Client/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="flex space-x-4 text-gray-600">
     <div class="shrink-0 p-8">
-        <NuxtLink to="/" class="uppercase font-interstate">Jasmin Scheidegger</NuxtLink>
+        <NuxtLink to="/" class="text-4xl font-light uppercase font-interstate">Jasmin Scheidegger</NuxtLink>
         <Navigation class="mt-8"></Navigation>
     </div>
     <div class="grow p-8">
